cade236a36e5f88d1804a2b5926c17a3f2ca8949
joaquinenriquez <joaquin.enriquez.tc@gmail.com>
2017-02-02 20:27:57 -0300

    Commit

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,10 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostar()
 {
-	
+	alert("Esto funciona de maravilla");
+	//Esto es un comentario de una línea
+	/*Esto es un comentario 
+	bloque
+	*/	
 }
-
+s
