6e2191faf054920b4e38330a34b817ee6276e329
joaquinenriquez <joaquin.enriquez.tc@gmail.com>
2017-02-02 22:16:45 -0300

    Segundo commint

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,12 @@ transformarlos a enteros (parseInt) y Sumarlos.
 Mostar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var nro1;
+	var nro2;
 
+	nro1 = parseInt(document.getElementById('numeroUno').value);
+	nro2 = parseInt(document.getElementById('numeroDos').value);
+
+	alert(nro1+nro2);
 }
 
